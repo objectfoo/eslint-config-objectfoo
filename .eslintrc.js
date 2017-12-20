eadaa370e52a329a77a231321e2d1481871fce94
Satake <satake@objectfoo.com>
2017-12-19 19:11:32 -0800

    my new config

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+	extends: [
+		'standard',
+		'standard-jsx'
+	],
+	rules: {
+		'semi': [2, 'always'],
+		'indent': [
+			'error',
+			'tab',
+			{'SwitchCase': 1}
+		],
+		'no-tabs': 0
+	}
+};
